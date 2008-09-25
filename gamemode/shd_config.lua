8a8e54af6fd228f9d5de27a858ca4f0641ef16e7
unniloct <unniloct@feb296b4-8670-11dd-8f23-d9c8888c2974>
2008-09-25 17:12:14 +0000

    [+] Added shd_config.lua, which was forgotten from the previous revision [!] Untested

---
+++ gamemode/shd_config.lua
@@ -0,0 +1,10 @@
+FALLFACTOR					=	2.0						// falling damage multiplier
+ROLLFACTOR					=	0.75					// falling damage multiplier when rolling
+SLIDEFACTOR					=	0.5						// gravity multiplier while sliding
+
+ALLOW = {
+	CLIMB					=	true,
+	WALLSLIDE				=	true,
+	WALLJUMP				=	true,
+	ROLL					=	true
+}
