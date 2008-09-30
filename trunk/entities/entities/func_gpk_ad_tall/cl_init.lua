36892455c679792dc516f5539be21fcff8018c1f
unniloct <unniloct@feb296b4-8670-11dd-8f23-d9c8888c2974>
2008-09-30 05:23:13 +0000

    [+] Added two maps: gpk_climbtest and gpk_lobby_1 (numbered for eventual release; I'm sticking with _1 for most of the pre-release development cycle) [+] Added some filler materials for ad space [+] Fixed some stuff

--- trunk/entities/entities/func_gpk_ad_tall/cl_init.lua
+++ trunk/entities/entities/func_gpk_ad_tall/cl_init.lua
@@ -6,8 +6,13 @@ function ENT:Draw()
 	cam.Start3D2D(self.Entity:GetPos() + self.Entity:GetForward() * 7, /*self.Entity:GetAngles()*/ Angle(180, 90, 270), 1.6)
 		local x = -40
 		local y = -120
-		surface.SetDrawColor(0, 0, 255, 255)
-		surface.DrawRect(x, y, 80, 240)
+		/*surface.SetDrawColor(0, 0, 255, 255)
+		surface.DrawRect(x, y, 80, 240)*/
+		surface.SetTexture(surface.GetTextureID("gpk/ad_tall_0")) 
+		surface.SetDrawColor(255,255,255,255)
+		//cam.IgnoreZ(true)
+		cam.IgnoreZ(false)
+		surface.DrawTexturedRect(x,y,256,256)
 		draw.SimpleText("80x240", "Fixedsys12", x+40, y+120, Color(255, 255, 255, 255), 1, 1)
 	cam.End3D2D()
 end
