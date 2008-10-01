f515a446ea48034dae90399c79156e9f0df7e1e5
unniloct <unniloct@feb296b4-8670-11dd-8f23-d9c8888c2974>
2008-10-01 00:58:51 +0000

    [~] Fixed all bugs with screens [!] Tested

--- entities/entities/func_gpk_pcscreen/cl_init.lua
+++ entities/entities/func_gpk_pcscreen/cl_init.lua
@@ -1,14 +1,14 @@
 include('shared.lua')
 function ENT:Initialize()
-	local h = 384
-	local l = 384 // TODO: FIX!!
+	local h = 32
+	local l = 24 // TODO: FIX!!
 	local w = 8
 	local _min = Vector(0-(w/2),0-(l/2),0-(h/2))
 	local _max = Vector(w/2,l/2,h/2) 
 	self.Entity:SetRenderBounds(_min,_max)
 end
 function ENT:Draw()
-	cam.Start3D2D(self.Entity:GetPos() - self.Entity:GetForward() * 1, /*self.Entity:GetAngles()*/ Angle(180, 270, 270), 0.1)
+	cam.Start3D2D(self.Entity:GetPos() - self.Entity:GetForward() * 0.5, /*self.Entity:GetAngles()*/ Angle(180, 270, 270), 0.1)
 		local x = -160
 		local y = -120
 		surface.SetDrawColor(0, 0, 255, 255)
--- entities/entities/func_gpk_pcscreen/shared.lua
+++ entities/entities/func_gpk_pcscreen/shared.lua
@@ -1,2 +1,5 @@
 ENT.Type = "anim"
 //ENT.Base = "base_gmodentity"
+function ENT:Think()
+	self.Entity:DrawShadow(false)
+end
