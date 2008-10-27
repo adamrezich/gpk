a58dbaa7975d945dc680f73e369c851c2ff5738f
unniloct <unniloct@feb296b4-8670-11dd-8f23-d9c8888c2974>
2008-10-27 06:31:36 +0000

    [+] Added new map, gpk_testcity. You should check it out, despite it being short and poorly-textured. [~] Shotguns now require a pump every other click. Slightly buggy, though. [~] Lots of bugfixes.

--- trunk/gamemode/shd_config.lua
+++ trunk/gamemode/shd_config.lua
@@ -1,6 +1,7 @@
 TEAMBASED					=	false					// make the gamemode team-based
 
 MAXSPEED					=	450						// player top speed
+WALKSPEED					=	200						// speed to walk slowly at
 
 WALLJUMPTIME				=	0.05
 WALLJUMPDECAY				=	0.05
@@ -8,6 +9,7 @@ WALLJUMPDECAY				=	0.05
 FALLFACTOR					=	2.0						// falling damage multiplier
 ROLLFACTOR					=	0.75					// falling damage multiplier when rolling
 WALLSLIDEFACTOR				=	0.9875					// gravity multiplier while wallsliding
+//WALLSLIDEFACTOR				=	0.9999					// gravity multiplier while wallsliding
 
 ALLOW = {												// whether or not to allow certain moves
 	CLIMB					=	true,
